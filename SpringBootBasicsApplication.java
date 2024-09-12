2aaad0c017269fb6b39c907002c4fbb015561512
SwethaB0706 <113886729+SwethaB0706@users.noreply.github.com>
2024-09-12 11:04:23 +0530

    spring app

---
+++ SpringBootBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.indium.Spring_Boot_Basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicsApplication.class, args);
+	}
+
+}
