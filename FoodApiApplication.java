df7b62a3de75a48d238060873eeb54687da926e6
Prabhu Sunderaraman <prabhu.bits@gmail.com>
2024-09-12 12:19:43 +0530

    Update FoodApiApplication.java

--- FoodApiApplication.java
+++ FoodApiApplication.java
@@ -1,13 +1,13 @@
-package com.indium.food_api;
+// package com.indium.food_api;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FoodApiApplication {
+// @SpringBootApplication
+// public class FoodApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodApiApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(FoodApiApplication.class, args);
+// 	}
 
-}
+// }
